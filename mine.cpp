01813178f63d33c3dcd263f9858244b2871d4745
Divyansh Garg <71733011+hash-brow@users.noreply.github.com>
2023-05-15 23:39:21 +0530

    Create mine.cpp

---
+++ mine.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#include "sha256.h"
+
+using namespace std;
+using namespace chrono;
+
+const string target = "00000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF";
+
+int main()
+{
+    string input;
+    getline(cin, input);
+
+    long nonce = 1;
+
+    auto begin = high_resolution_clock::now();
+
+    while(true){
+        string temp = input + to_string(nonce);
+        string hash = sha256(temp);
+
+        for(char &c : hash)
+            c = (char)toupper(c);
+
+        if(hash < target){
+            cout << hash << " " << nonce << "\n";
+            break;
+        }
+
+        nonce++;
+    }
+
+    auto end = high_resolution_clock::now();
+    cout << fixed << setprecision(6);
+    cout << "Execution Time: " << duration_cast<duration<double>>(end - begin).count() << "seconds" << endl;
+
+    return 0;
+}
